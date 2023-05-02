e3a8c73d931e342387a6485ff8ed9b1eab6ff637
yhyu13 <yohan680919@gmail.com>
2023-05-03 00:56:59 +0800

    Update script

--- docker_build_whl.sh
+++ docker_build_whl.sh
@@ -1,9 +1,12 @@
-docker build --network host -t gptq-llama-cuda .
-docker run gptq-llama-cuda
+#!/bin/bash
+
+# fill in the cuda arch list
+TORCH_CUDA_ARCH_LIST="8.6"
+
+docker build --build-arg TORCH_CUDA_ARCH_LIST=$TORCH_CUDA_ARCH_LIST --network host -t gptq-llama-cuda .
+docker run -d --name gptq-llama-cuda-container gptq-llama-cuda
 docker ps -all
 
-DOCKER_CONTAINER_ID=$(docker ps -all -q | head -n 1)
-# Then find the docker Container ID and
-docker cp $DOCKER_CONTAINER_ID:/result .
-docker stop $DOCKER_CONTAINER_ID
-docker rm $DOCKER_CONTAINER_ID
+DOCKER_CONTAINER_ID=$(docker ps -aqf "name=gptq-llama-cuda-container")
+# Then find the docker Container ID and copy the result
+docker cp $DOCKER_CONTAINER_ID:/result . && docker stop $DOCKER_CONTAINER_ID && docker rm $DOCKER_CONTAINER_ID
